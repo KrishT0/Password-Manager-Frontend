52af5d3084823ed5294a5fb41b050d703c0cb0cb
KrishT0 <krshnabiswal619@gmail.com>
2024-10-21 11:41:13 +0530

    added grid background to auth page

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,13 +1,23 @@
+import SignIn from "@/pages/auth/SignIn";
+import SignUp from "@/pages/auth/SignUp";
+import GridPattern from "@/components/custom/animated-grid-background";
 
 //shadcn Ui imports
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
 
 function AuthPage() {
   return (
     <div className="flex h-screen w-screen justify-center items-center px-1">
-      <Tabs defaultValue="sign-in" className="w-[450px]">
+      <GridPattern
+        width={25}
+        height={25}
+        maxOpacity={0.07}
+        x={-1}
+        y={-1}
+        strokeDasharray={"5 4"}
+        className="[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]"
+      />
+      <Tabs defaultValue="sign-in" className="w-[450px] z-10">
         <TabsList className="w-full">
           <TabsTrigger value="sign-in" className="w-1/2">
             Sign In
@@ -16,10 +26,10 @@ function AuthPage() {
             Sign Up
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="sign-in">
+        <TabsContent value="sign-in" className="z-10 bg-background">
           <SignIn />
         </TabsContent>
-        <TabsContent value="sign-up">
+        <TabsContent value="sign-up" className="z-10 bg-background">
           <SignUp />
         </TabsContent>
       </Tabs>
